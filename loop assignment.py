20e5b290a38c83b456ba536b8d1ab442d4d99923
AswaniBhavanasi <yashaswani649@gmail.com>
2024-08-30 12:35:30 +0530

    for loop programs

---
+++ loop assignment.py
@@ -0,0 +1,78 @@
+"""lst=[10,20,30,40,50,60,70]
+for i in lst:
+    print(i)
+    print(i,end='\n')
+    print(i,end=',')"""
+
+'''st='aswani'
+for i in st:
+    print([i],end=',')'''
+
+'''emp={'ename':'sai','sal':60000,'comp':'info'}
+for i in emp:
+    print(emp[i])'''
+
+#write a program to generate the numbers from 1 to 9
+'''for i in range(1,9,1):
+    print(i)'''
+
+#write a program to generate the even numbers from  2 to 30
+'''for i in range(2,31,2):
+    print(i)'''
+
+#write a program to generate the odd numbers from 11 to 41
+'''for i in range(11,41,2):
+    print(i)'''
+#write a program to generate the  all 5 multiples from  5 to 36
+'''for i in range(5,36,5):
+    print(i)'''
+# write a program to generate the numbers from -1 to 10
+'''for i in range(10,0,-1):
+    print(i)'''
+#write a program to genrate the numbers from -10 to -1
+'''for i in range(-10,-1,1):
+    print(i)'''
+#write a program to generate the numbers from -10 to 1
+'''for i in range(-10,1,1):
+    print(i)'''
+# write a program to generate the numbers from -5 to 5
+'''for i in range(-5 ,6,1):
+    print(i)'''
+# write a program to generate the all even numbers from -10 to 10
+'''for i in range(-10,11,2):
+    print(i)'''
+#write a program to fetch all even numbers  from list
+'''lst=[10,12,13,14,16,17,19,20,23]
+newlst=[]
+for i in lst:
+    if i %2==0:
+        newlst.append(i)
+print(newlst)'''
+# write a program to fetch all words which starts with vowel
+'''names=['sai','ashu','isu','vishnu','sam','ajay','mani']
+newlst=[]
+for i in names:
+    if i[0] in 'AEIOUaeiou':
+        newlst.append(i)
+print(newlst)'''
+#write a program to fetch all words which contains only 6 characters
+'''st='python is a simple and general purpose language'
+newlst=[]
+for i in st.split(' '):
+    if len(i)==6:
+        newlst.append(i)
+print(newlst) '''
+
+#write a program to display the table as per user requirement
+
+'''n=eval(input('enter any number:'))
+for i in range(1,11,1):
+    print(n,'*',i,'=',n * i)'''
+    
+
+
+    
+
+
+    
+
